b836a08bff4153d01e2527a2fb698b1ccf5253d9
JayRodButray <jayrodbutray@gmail.com>
2023-06-19 21:16:04 -0700

    add first parts of html,css,js

--- assets/style.js
+++ assets/style.js
@@ -2,15 +2,12 @@
 //add weather api key
     var apiKey = '67e13df9856c16c7a85cf522557bfa6d';
 
-//add one city to start to check for functionality
-    var city = 'Tehachapi';
-
 //api url from which to fetch data
-    var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={67e13df9856c16c7a85cf522557bfa6d}"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast
 
 //first function to fetch weather location, temp and description
     function getWeather() {
-      fetch(url)
+      fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
           var locationElement = document.getElementById('location');
