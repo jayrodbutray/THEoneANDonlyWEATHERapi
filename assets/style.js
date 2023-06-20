77f201f27a31714e0f69bbda98881be3ba2c5892
JayRodButray <jayrodbutray@gmail.com>
2023-06-19 23:49:17 -0700

    fixed forecast url on js

--- assets/style.js
+++ assets/style.js
@@ -3,7 +3,7 @@
     var apiKey = '67e13df9856c16c7a85cf522557bfa6d';
 
 //api url from which to fetch data
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast"
 
 //first function to fetch weather location, temp and description
     function getWeather() {
