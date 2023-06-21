e7959e9e3ef7ce30675552086e012a86c75ddfca
JayRodButray <jayrodbutray@gmail.com>
2023-06-20 19:24:56 -0700

    fixed API 401 error

--- assets/style.js
+++ assets/style.js
@@ -1,26 +1,62 @@
+var timeEl = document.getElementById("time");
+var dateEl = document.getElementById("date");
+var currentWeatherItemsEl = document.getElementById("current-weather-items");
+var timezone = document.getElementById("time-zone");
+var countryEl = document.getElementById("country");
+var weatherForecastEl = document.getElementById("weather-forecast");
+var currentTempEl = document.getElementById("current-temp");
 
-//add weather api key
-    var apiKey = '67e13df9856c16c7a85cf522557bfa6d';
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-//api url from which to fetch data
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast"
 
-//first function to fetch weather location, temp and description
-    function getWeather() {
-      fetch(apiUrl)
+setInterval(() => {
+    var time = new Date();
+    var month = time.getMonth();
+    var date = time.getDate();
+    var day = time.getDay();
+    var hour = time.getHours();
+    var hoursIn24HrFormat;
+if (hour >= 12 && hour < 24) {
+  hoursIn24HrFormat = hour;
+} else if (hour >= 0 && hour < 12) {
+  hoursIn24HrFormat = hour + 12;
+}
+console.log("hoursIn24HrFormat");
+ var minutes = time.getMinutes();
+    var ampm;
+if (hour >= 12) {
+  ampm = "PM";
+} else {
+  ampm = "AM";
+}
+
+timeEl.innerHTML = `${hoursIn24HrFormat}:${minutes} <span id="am-pm">${ampm}</span>`;
+
+dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month];
+
+}, 1000);
+
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+      let { latitude, longitude } = success.coords;
+  
+      var API_KEY = 'e0948e49398ecf759c545ec952d5f846';
+  
+      var url = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`;
+  
+      fetch(url)
         .then(response => response.json())
         .then(data => {
-          var locationElement = document.getElementById('location');
-          var temperatureElement = document.getElementById('temperature');
-          var descriptionElement = document.getElementById('description');
-
-//code to update and make sure there are no errors
-          locationElement.textContent = `Location: ${data.location.name}, ${data.location.country}`;
-          temperatureElement.textContent = `Temperature: ${data.current.temp_c}°C`;
-          descriptionElement.textContent = `Description: ${data.current.condition.text}`;
+          // Process the retrieved weather data
+          console.log(data);
         })
-        .catch(error => console.log(error));
-    }
-
-//function call to get above code to display needed info.
-    getWeather();
+        .catch(error => {
+          // Handle any errors
+          console.log('Error:', error);
+        });
+    });
+  }
+  
+  getWeatherData();
+  
